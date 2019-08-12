3b24d1ca6f06658ee7f3324204a51d1ace7e5d49
liaocaisong <liaocaisong>
2019-08-12 15:13:57 +0800

    修改本地dev

--- demo/src/main/java/com/springboot/service/HelloWorld.java
+++ demo/src/main/java/com/springboot/service/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.springboot.service;
 
 
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("jejdj");
         System.out.println("ddfd");
+        System.out.println("修改本地分支dev/xiaosong");
     }
 }
